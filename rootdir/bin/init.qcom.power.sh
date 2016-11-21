bfe77fe040937ca212e45d9ba033da7c2231a3dd
jwchen119 <jwchen119@gmail.com>
2016-11-21 21:50:18 +0800

    Fix thermanager fetal error, enable core_ctl

--- rootdir/bin/init.qcom.power.sh
+++ rootdir/bin/init.qcom.power.sh
@@ -20,6 +20,12 @@ function get-set-forall() {
 
 ################################################################################
 
+# take the A57s offline when thermal hotplug is disabled
+write /sys/devices/system/cpu/cpu4/online 0
+write /sys/devices/system/cpu/cpu5/online 0
+write /sys/devices/system/cpu/cpu6/online 0
+write /sys/devices/system/cpu/cpu7/online 0
+
 # disable thermal bcl hotplug to switch governor
 write /sys/module/msm_thermal/core_control/enabled 0
 get-set-forall /sys/devices/soc.0/qcom,bcl.*/mode disable
@@ -32,18 +38,21 @@ get-set-forall /sys/devices/soc.0/qcom,bcl.*/mode enable
 # Restorecon again to give new files the correct label.
 restorecon -R /sys/devices/system/cpu
 
-# ensure at most one A57 is online when thermal hotplug is disabled
-write /sys/devices/system/cpu/cpu5/online 0
-write /sys/devices/system/cpu/cpu6/online 0
-write /sys/devices/system/cpu/cpu7/online 0
+# Disable CPU retention
+write /sys/module/lpm_levels/system/a53/cpu0/retention/idle_enabled 0
+write /sys/module/lpm_levels/system/a53/cpu1/retention/idle_enabled 0
+write /sys/module/lpm_levels/system/a53/cpu2/retention/idle_enabled 0
+write /sys/module/lpm_levels/system/a53/cpu3/retention/idle_enabled 0
+write /sys/module/lpm_levels/system/a57/cpu4/retention/idle_enabled 0
+write /sys/module/lpm_levels/system/a57/cpu5/retention/idle_enabled 0
+write /sys/module/lpm_levels/system/a57/cpu6/retention/idle_enabled 0
+write /sys/module/lpm_levels/system/a57/cpu7/retention/idle_enabled 0
 
-# Best effort limiting for first time boot if msm_performance module is absent
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 960000
+# Disable L2 retention
+write /sys/module/lpm_levels/system/a53/a53-l2-retention/idle_enabled 0
+write /sys/module/lpm_levels/system/a57/a57-l2-retention/idle_enabled 0
 
-# Limit A57 max freq from msm_perf module in case CPU 4 is offline
-write /sys/module/msm_performance/parameters/cpu_max_freq "4:960000 5:960000 6:960000 7:960000"
-
-# configure governor settings for little cluster
+# Setup Little interactive settings
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor interactive
 restorecon -R /sys/devices/system/cpu # must restore after interactive
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load 1
@@ -56,65 +65,8 @@ write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 1
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "65 460000:75 960000:80"
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 40000
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 80000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/ignore_hispeed_on_notif 1
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 384000
-
-# online CPU4
-write /sys/devices/system/cpu/cpu4/online 1
-
-# configure governor settings for big cluster
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor interactive
-restorecon -R /sys/devices/system/cpu # must restore after interactive
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay 19000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 99
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 19000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 1248000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "70 960000:80 1248000:85"
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 40000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 80000
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 633600
-
-# restore A57's max
-copy /sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_max_freq /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
-
-# plugin remaining A57s
-write /sys/devices/system/cpu/cpu5/online 1
-write /sys/devices/system/cpu/cpu6/online 1
-write /sys/devices/system/cpu/cpu7/online 1
-
-# Restore CPU 4 max freq from msm_performance
-write /sys/module/msm_performance/parameters/cpu_max_freq "4:4294967295 5:4294967295 6:4294967295 7:4294967295"
-
-# input boost configuration
-write /sys/module/cpu_boost/parameters/input_boost_freq "0:1344000"
-write /sys/module/cpu_boost/parameters/input_boost_ms 40
-
-# Setting B.L scheduler parameters
-write /proc/sys/kernel/sched_migration_fixup 1
-write /proc/sys/kernel/sched_upmigrate 95
-write /proc/sys/kernel/sched_downmigrate 85
-write /proc/sys/kernel/sched_freq_inc_notify 400000
-write /proc/sys/kernel/sched_freq_dec_notify 400000
-
-# android background processes are set to nice 10. Never schedule these on the a57s.
-write /proc/sys/kernel/sched_upmigrate_min_nice 9
-
-get-set-forall  /sys/class/devfreq/qcom,cpubw*/governor bw_hwmon
-
-# Disable sched_boost
-write /proc/sys/kernel/sched_boost 0
-
-# re-enable thermal and BCL hotplug
-write /sys/module/msm_thermal/core_control/enabled 1
-get-set-forall /sys/devices/soc.0/qcom,bcl.*/mode disable
-get-set-forall /sys/devices/soc.0/qcom,bcl.*/hotplug_mask $bcl_hotplug_mask
-get-set-forall /sys/devices/soc.0/qcom,bcl.*/hotplug_soc_mask $bcl_hotplug_soc_mask
-get-set-forall /sys/devices/soc.0/qcom,bcl.*/mode enable
-
-# change GPU initial power level from 305MHz(level 4) to 180MHz(level 5) for power savings
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 5
 
 # Make sure CPU 4 is only to configure big settings
 write /sys/devices/system/cpu/cpu4/online 1
@@ -131,10 +83,10 @@ write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 19000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 1248000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 1
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "70 960000:80 1248000:85"
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 39000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 79000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 40000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 80000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/ignore_hispeed_on_notif 1
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 384000
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 633600
 
 # Configure core_ctl
 write /sys/devices/system/cpu/cpu4/core_ctl/min_cpus 1
@@ -153,11 +105,11 @@ write /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms 100
 write /sys/devices/system/cpu/cpu0/core_ctl/not_preferred 1
 
 # Available Freqs in stock kernel
-# Little: 384000 460800 600000 672000 787200 864000 960000 1248000 1440000
-# Big: 384000 480000 633600 768000 864000 960000 1248000 1344000 1440000 1536000 1632000 1689600 1824000
+# Little: 384000 460800 600000 672000 768000 864000 960000 1248000 1344000 1478400 1555200
+# Big: 384000 480000 633600 768000 864000 960000 1248000 1344000 1440000 1536000 1632000 1728000 1824000 1948400
 write /sys/module/cpu_boost/parameters/boost_ms 20
 write /sys/module/cpu_boost/parameters/sync_threshold 960000
-write /sys/module/cpu_boost/parameters/input_boost_freq 0:787200
+write /sys/module/cpu_boost/parameters/input_boost_freq 0:1344000
 write /sys/module/cpu_boost/parameters/input_boost_ms 40
 
 # b.L scheduler parameters
@@ -165,8 +117,8 @@ write /proc/sys/kernel/sched_migration_fixup 1
 write /proc/sys/kernel/sched_small_task 30
 write /proc/sys/kernel/sched_mostly_idle_load 20
 write /proc/sys/kernel/sched_mostly_idle_nr_run 3
-write /proc/sys/kernel/sched_downmigrate 50
-write /proc/sys/kernel/sched_upmigrate 70
+write /proc/sys/kernel/sched_upmigrate 95
+write /proc/sys/kernel/sched_downmigrate 85
 write /proc/sys/kernel/sched_init_task_load 50
 write /proc/sys/kernel/sched_freq_inc_notify 400000
 write /proc/sys/kernel/sched_freq_dec_notify 400000
